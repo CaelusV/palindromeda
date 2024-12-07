59b93bb566e020730acf337ada13e71a49ac3585
CaelusV <39420165+CaelusV@users.noreply.github.com>
2024-12-07 01:08:03 +0100

    impl PartialEq, PartialOrd between Palindrome and u64

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Palindrome(u64);
 
 impl Palindrome {
     /// Construct a palindrome from the first half of a digit and a provided length.
-    /// Will panic if 'length' isn't 2x or 2x - 1 the size of 'digits_half.len()'.
+    ///
+    /// NOTE: Will panic if `length` isn't `2x` or `2x - 1` the size of `digits_half.len()`.
     fn construct_palindrome(length: usize, digits_half: &[u8]) -> Self {
         assert_eq!(
             length.div_ceil(2),
@@ -48,18 +49,20 @@ impl Palindrome {
         (digits, length)
     }
 
-    /// Return the first palindromic number that is smaller.
-    /// NOTE: Lowest return-value is '0'.
-    pub fn decrement(&self) -> Self {
+    /// Return the previous palindromic number.
+    ///
+    /// **NOTE:** Lowest return-value is `0`.
+    pub fn previous(&self) -> Self {
         if self.0 == 0 {
             return Self(0);
         }
         Self::le(self.0 - 1)
     }
 
-    /// Return the first palindromic number that is larger.
-    /// NOTE: Highest return-value is the first palindromic number less than 'u64::MAX'.
-    pub fn increment(&self) -> Self {
+    /// Return the next palindromic number.
+    ///
+    /// **NOTE:** Highest return-value is the first palindromic number less than [`u64::MAX`].
+    pub fn next(&self) -> Self {
         // Largest possible palindrome to fit in u64.
         if self.0 >= 18446744066044764481 {
             return Self(self.0);
@@ -67,12 +70,12 @@ impl Palindrome {
         Self::ge(self.0 + 1)
     }
 
-    /// Return whether 'x' is a palindrome. Same as 'is_palindromic()'.
+    /// Return whether `x` is a palindrome. Same as [`Self::is_palindromic()`].
     pub fn is_palindrome(x: u64) -> bool {
         Self::is_palindromic(x)
     }
 
-    /// Return whether 'x' is palindromic. Same as 'is_palindrome()'.
+    /// Return whether `x` is palindromic. Same as [`Self::is_palindrome()`].
     pub fn is_palindromic(mut x: u64) -> bool {
         if x % 10 == 0 && x != 0 {
             return false;
@@ -87,7 +90,7 @@ impl Palindrome {
         return x == right_half || x == right_half / 10;
     }
 
-    /// Return the first palindromic number that is less than or equal to 'x'.
+    /// Return the first palindromic number that is less than or equal to `x`.
     pub fn le(x: u64) -> Self {
         if Self::is_palindromic(x) {
             return Palindrome(x);
@@ -146,7 +149,7 @@ impl Palindrome {
         palindrome
     }
 
-    /// Return the first palindromic number that is greater than or equal to 'x'.
+    /// Return the first palindromic number that is greater than or equal to `x`.
     pub fn ge(x: u64) -> Self {
         // Largest possible palindrome to fit in u64.
         // FIXME: This is returning a palindromic number LESS than 'x'.
@@ -207,104 +210,187 @@ impl From<Palindrome> for u64 {
     }
 }
 
+impl PartialEq<u64> for Palindrome {
+    fn eq(&self, other: &u64) -> bool {
+        self.0 == *other
+    }
+}
+
+impl PartialEq<Palindrome> for u64 {
+    fn eq(&self, other: &Palindrome) -> bool {
+        self == other
+    }
+}
+
+impl PartialOrd<u64> for Palindrome {
+    fn ge(&self, other: &u64) -> bool {
+        self.0 >= *other
+    }
+
+    fn gt(&self, other: &u64) -> bool {
+        self.0 > *other
+    }
+
+    fn le(&self, other: &u64) -> bool {
+        self.0 <= *other
+    }
+
+    fn lt(&self, other: &u64) -> bool {
+        self.0 < *other
+    }
+
+    fn partial_cmp(&self, other: &u64) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(other)
+    }
+}
+
+impl PartialOrd<Palindrome> for u64 {
+    fn ge(&self, other: &Palindrome) -> bool {
+        self >= other
+    }
+
+    fn gt(&self, other: &Palindrome) -> bool {
+        self > other
+    }
+
+    fn le(&self, other: &Palindrome) -> bool {
+        self <= other
+    }
+
+    fn lt(&self, other: &Palindrome) -> bool {
+        self < other
+    }
+
+    fn partial_cmp(&self, other: &Palindrome) -> Option<std::cmp::Ordering> {
+        self.partial_cmp(&other.0)
+    }
+}
+
+pub struct PalindromeIter {
+    from: Palindrome,
+    to: Palindrome,
+}
+
+impl PalindromeIter {
+    pub fn from(from: u64, to: u64) -> Self {
+        Self {
+            from: Palindrome::ge(from),
+            // If it's not a palindrome, then we want to include the previous palindrome.
+            to: Palindrome(to),
+        }
+    }
+}
+
+impl Iterator for PalindromeIter {
+    type Item = Palindrome;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let return_value = self.from;
+        self.from = self.from.next();
+
+        if return_value < self.to {
+            return Some(return_value);
+        } else {
+            return None;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Palindrome;
 
     #[test]
     fn test_construct_palindrome() {
-        assert_eq!(34543, Palindrome::construct_palindrome(5, &vec![3, 4, 5]).0);
-        assert_eq!(
-            345543,
-            Palindrome::construct_palindrome(6, &vec![3, 4, 5]).0
-        );
-        assert_eq!(0, Palindrome::construct_palindrome(1, &vec![0]).0);
-        assert_eq!(0, Palindrome::construct_palindrome(2, &vec![0]).0);
+        assert_eq!(34543, Palindrome::construct_palindrome(5, &vec![3, 4, 5]));
+        assert_eq!(345543, Palindrome::construct_palindrome(6, &vec![3, 4, 5]));
+        assert_eq!(0, Palindrome::construct_palindrome(1, &vec![0]));
+        assert_eq!(0, Palindrome::construct_palindrome(2, &vec![0]));
         assert_eq!(
             1710171,
-            Palindrome::construct_palindrome(7, &vec![1, 7, 1, 0]).0
+            Palindrome::construct_palindrome(7, &vec![1, 7, 1, 0])
         );
         assert_eq!(
             17100171,
-            Palindrome::construct_palindrome(8, &vec![1, 7, 1, 0]).0
+            Palindrome::construct_palindrome(8, &vec![1, 7, 1, 0])
         );
     }
 
     #[test]
     #[should_panic]
     fn test_construct_palindrome_panic_on_too_short_length() {
-        assert_eq!(34543, Palindrome::construct_palindrome(4, &vec![3, 4, 5]).0);
+        assert_eq!(34543, Palindrome::construct_palindrome(4, &vec![3, 4, 5]));
     }
 
     #[test]
     #[should_panic]
     fn test_construct_palindrome_panic_on_too_big_length() {
-        assert_eq!(34543, Palindrome::construct_palindrome(7, &vec![3, 4, 5]).0);
+        assert_eq!(34543, Palindrome::construct_palindrome(7, &vec![3, 4, 5]));
     }
 
     #[test]
-    fn test_palindrome_decrement() {
+    fn test_palindrome_previous() {
         let pal = Palindrome(22);
-        assert_eq!(11, pal.decrement().0);
+        assert_eq!(11, pal.previous());
         let pal = Palindrome(998899);
-        assert_eq!(997799, pal.decrement().0);
+        assert_eq!(997799, pal.previous());
         let pal = Palindrome(212);
-        assert_eq!(202, pal.decrement().0);
+        assert_eq!(202, pal.previous());
         let pal = Palindrome(202);
-        assert_eq!(191, pal.decrement().0);
+        assert_eq!(191, pal.previous());
         let pal = Palindrome(191);
-        assert_eq!(181, pal.decrement().0);
+        assert_eq!(181, pal.previous());
         let pal = Palindrome(1991);
-        assert_eq!(1881, pal.decrement().0);
+        assert_eq!(1881, pal.previous());
         let pal = Palindrome(100001);
-        assert_eq!(99999, pal.decrement().0);
+        assert_eq!(99999, pal.previous());
         let pal = Palindrome(1001);
-        assert_eq!(999, pal.decrement().0)
+        assert_eq!(999, pal.previous())
     }
 
     #[test]
-    fn test_palindrome_increment() {
+    fn test_palindrome_next() {
         let pal = Palindrome(22);
-        assert_eq!(33, pal.increment().0);
+        assert_eq!(33, pal.next());
         let pal = Palindrome(998899);
-        assert_eq!(999999, pal.increment().0);
+        assert_eq!(999999, pal.next());
         let pal = Palindrome(999999);
-        assert_eq!(1000001, pal.increment().0);
+        assert_eq!(1000001, pal.next());
         let pal = Palindrome(212);
-        assert_eq!(222, pal.increment().0);
+        assert_eq!(222, pal.next());
         let pal = Palindrome(191);
-        assert_eq!(202, pal.increment().0);
+        assert_eq!(202, pal.next());
         let pal = Palindrome(181);
-        assert_eq!(191, pal.increment().0);
+        assert_eq!(191, pal.next());
         let pal = Palindrome(1881);
-        assert_eq!(1991, pal.increment().0);
+        assert_eq!(1991, pal.next());
     }
 
     #[test]
     fn test_palindrome_le() {
-        assert_eq!(9, Palindrome::le(10).0);
-        assert_eq!(11, Palindrome::le(11).0);
-        assert_eq!(11, Palindrome::le(19).0);
-        assert_eq!(99, Palindrome::le(100).0);
-        assert_eq!(997799, Palindrome::le(998001).0);
-        assert_eq!(202, Palindrome::le(209).0);
-        assert_eq!(191, Palindrome::le(201).0);
-        assert_eq!(181, Palindrome::le(190).0);
-        assert_eq!(1881, Palindrome::le(1990).0);
-        assert_eq!(99999, Palindrome::le(100000).0);
-        assert_eq!(999, Palindrome::le(1000).0)
+        assert_eq!(9, Palindrome::le(10));
+        assert_eq!(11, Palindrome::le(11));
+        assert_eq!(11, Palindrome::le(19));
+        assert_eq!(99, Palindrome::le(100));
+        assert_eq!(997799, Palindrome::le(998001));
+        assert_eq!(202, Palindrome::le(209));
+        assert_eq!(191, Palindrome::le(201));
+        assert_eq!(181, Palindrome::le(190));
+        assert_eq!(1881, Palindrome::le(1990));
+        assert_eq!(99999, Palindrome::le(100000));
+        assert_eq!(999, Palindrome::le(1000))
     }
 
     #[test]
     fn test_palindrome_ge() {
-        assert_eq!(11, Palindrome::ge(10).0);
-        assert_eq!(11, Palindrome::ge(11).0);
-        assert_eq!(22, Palindrome::ge(19).0);
-        assert_eq!(101, Palindrome::ge(100).0);
-        assert_eq!(998899, Palindrome::ge(998001).0);
-        assert_eq!(212, Palindrome::ge(209).0);
-        assert_eq!(202, Palindrome::ge(199).0);
-        assert_eq!(191, Palindrome::ge(190).0);
-        assert_eq!(1991, Palindrome::ge(1990).0);
+        assert_eq!(11, Palindrome::ge(10));
+        assert_eq!(11, Palindrome::ge(11));
+        assert_eq!(22, Palindrome::ge(19));
+        assert_eq!(101, Palindrome::ge(100));
+        assert_eq!(998899, Palindrome::ge(998001));
+        assert_eq!(212, Palindrome::ge(209));
+        assert_eq!(202, Palindrome::ge(199));
+        assert_eq!(191, Palindrome::ge(190));
+        assert_eq!(1991, Palindrome::ge(1990));
     }
 }
